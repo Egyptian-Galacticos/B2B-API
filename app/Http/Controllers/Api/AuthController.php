d2d3a6dc1c70abe998a1ae927771d9609fb1e711
anas nashat <aanasht@gmail.com>
2025-05-31 20:08:27 +0300

    feat(AuthController): implement login, register and me endpoints- Add login method with JWT authentication- Implement user registration with password hashing- Create me method to return authenticated user information- Add necessary imports and namespace declaration

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = auth()->login($user);
+
+            return response()->json(
+                [
+                    'access_token' => $token,
+                    'token_type' => 'bearer',
+                    'expires_in' => auth()->factory()->getTTL() * 60], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        // Generate JWT token
+        $token = auth()->login($user);
+
+        return response()->json([
+            'message' => 'Registered successfully',
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ], 201);
+    }
+
+    public function me()
+    {
+        if (! auth()->check()) {
+            return response()->json(['error' => 'Unauthenticated'], 401);
+        }
+
+        return response()->json(auth()->user(), 200);
+    }
 }
