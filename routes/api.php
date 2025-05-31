d5075cd02432150c83ef54d6e1f687e4804b520f
anas nashat <aanasht@gmail.com>
2025-05-31 20:08:05 +0300

    feat(routes): add auth routes for API- Remove default user route- Add auth routes for login, register, and get user information- Use AuthController for handling auth requests

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+    Route::get('me', [AuthController::class, 'me'])->name('auth.me');
+});
