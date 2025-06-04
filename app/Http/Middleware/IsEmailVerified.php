aaefae26c5741958f872d8a5c61055d887b18809
anas nashat <aanasht@gmail.com>
2025-06-04 18:30:58 +0300

    feat(auth): implement email verification functionality- Add IsEmailVerified middleware to check if user's email is verified- Create VerifyEmail notification to send verification link to users

---
+++ app/Http/Middleware/IsEmailVerified.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Traits\ApiResponse;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsEmailVerified
+{
+    use ApiResponse;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $user = auth()->user();
+        if ($user && ! $user->is_email_verified) {
+            return $this->apiResponse(null, 'Email not verified', 403);
+        }
+
+        return $next($request);
+    }
+}
